3d0693be4a4384fa1ce123e225907f8a609d20e0
jones58 <108466184+jones58@users.noreply.github.com>
2024-05-24 16:04:14 +0100

    setting up the basic structure of the site

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,3 +1,10 @@
 export default function Location() {
-  return <div className="p-10 text-2xl lg:p-20 lg:text-4xl">Hello</div>;
+  return (
+    <div
+      className="min-h-screen p-10 text-2xl lg:p-20 lg:text-4xl"
+      id="location"
+    >
+      Hello
+    </div>
+  );
 }
--- src/components/WhatsOn.tsx
+++ src/components/WhatsOn.tsx
@@ -1,3 +1,10 @@
 export default function WhatsOn() {
-  return <div className="p-10 text-2xl lg:p-20 lg:text-4xl">Hello</div>;
+  return (
+    <div
+      className="min-h-screen  p-10 text-2xl lg:p-20 lg:text-4xl"
+      id="whatsOn"
+    >
+      Hello
+    </div>
+  );
 }
