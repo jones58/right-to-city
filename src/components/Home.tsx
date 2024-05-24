c66e73c0aa9398a6e0deaecddfd6886454008264
jones58 <108466184+jones58@users.noreply.github.com>
2024-05-24 16:14:54 +0100

    edit readme with progress and set up components

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,29 +1,31 @@
 export default function Home() {
   return (
-    <div className="grid h-screen grid-rows-4" id="home">
-      <div className="row-span-3 lg:grid lg:grid-cols-5">
-        <h1 className="relative z-20 mt-20 p-10 pt-20 text-5xl font-bold text-white lg:col-span-3 lg:p-20 lg:text-8xl">
-          Our Right to the City: <br /> A Festival of Urban Liberation
-        </h1>
+    <div>
+      <div className="grid h-screen grid-rows-4 " id="home">
+        <div className="row-span-3 lg:grid lg:grid-cols-5">
+          <h1 className="relative z-20 mt-20 p-10 pt-20 text-5xl font-bold text-white lg:col-span-3 lg:p-20 lg:text-8xl">
+            Our Right to the City: <br /> A Festival of Urban Liberation
+          </h1>
+        </div>
+        <div className="z-20 row-span-1 flex flex-col justify-start bg-black p-10 text-3xl text-white lg:row-span-1 lg:flex-row lg:justify-end lg:space-x-10 lg:space-y-0 lg:px-20 lg:text-4xl">
+          <a href="#about" className="decoration-wavy hover:underline">
+            About
+          </a>
+          <a href="#whatsOn" className="decoration-wavy hover:underline">
+            What's On
+          </a>
+          <a href="#location" className="decoration-wavy hover:underline">
+            Location
+          </a>
+          <a
+            href="#getInvolved"
+            className="decoration-wavy hover:underline"
+          >
+            Get Involved
+          </a>
+        </div>
+        <div className="absolute inset-0 z-0 h-screen bg-[url('images/city-bg.jpg')] bg-cover bg-center brightness-50"></div>
       </div>
-      <div className="z-20 row-span-1 flex flex-col justify-start bg-black p-10 text-3xl text-white lg:row-span-1 lg:flex-row lg:justify-end lg:space-x-10 lg:space-y-0 lg:px-20 lg:text-4xl">
-        <a href="#about" className="decoration-wavy hover:underline">
-          About
-        </a>
-        <a href="#whatsOn" className="decoration-wavy hover:underline">
-          What's On
-        </a>
-        <a href="#location" className="decoration-wavy hover:underline">
-          Location
-        </a>
-        <a
-          href="#getInvolved"
-          className="decoration-wavy hover:underline"
-        >
-          Get Involved
-        </a>
-      </div>
-      <div className="absolute inset-0 z-0 h-screen bg-[url('images/city-bg.jpg')] bg-cover bg-center brightness-50"></div>
     </div>
   );
 }
--- src/components/WhatsOn.tsx
+++ src/components/WhatsOn.tsx
@@ -1,10 +1,11 @@
 export default function WhatsOn() {
   return (
     <div
-      className="min-h-screen  p-10 text-2xl lg:p-20 lg:text-4xl"
-      id="whatsOn"
+      className="min-h-screen p-10 text-2xl lg:grid lg:grid-cols-5 lg:p-20 lg:text-4xl"
+      id="about"
     >
-      Hello
+      <div className="col-span-3">Hello</div>
+      <div className="col-span-2">Hello</div>
     </div>
   );
 }
