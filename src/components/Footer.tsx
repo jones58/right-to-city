1cd17d52cde443f2f2d19b3ac1273d26dc1ce2f3
jones58 <108466184+jones58@users.noreply.github.com>
2024-05-25 17:28:41 +0100

    edit footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <p className="font-sans">
         Our Right to the City: A Festival of Urban Liberation
       </p>
-      <div className="py-10 lg:py-0">
+      <div className="py-10 lg:py-0 lg:text-right">
         <p>
           Site by{' '}
           <a
