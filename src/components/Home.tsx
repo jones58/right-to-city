5acd79b33e4ea6801e2e27cca5edef3a9f630033
jones58 <108466184+jones58@users.noreply.github.com>
2024-05-25 16:57:59 +0100

    improving mobile and desktop layout

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,42 +1,35 @@
 export default function Home() {
   return (
-    <div>
-      <div className="grid h-screen grid-rows-4 " id="home">
-        <div className="row-span-3 lg:grid lg:grid-cols-5">
-          <div className="relative z-20 mt-20 p-10 pt-20 text-5xl font-bold text-white lg:col-span-3 lg:p-20 lg:text-7xl">
-            <h1>
-              Our Right to the City: <br /> A Festival of Urban Liberation
-            </h1>
-            <a
-              href="#getInvolved"
-              className="relative z-20 mt-20 pt-20 text-3xl font-normal  hover:opacity-70 active:opacity-100 lg:col-span-2 lg:text-6xl"
-            >
-              Get Involved
-            </a>
-          </div>
+    <div
+      id="home"
+      className="relative grid h-screen grid-rows-5 lg:grid-rows-4"
+    >
+      <div className="row-span-3 lg:row-span-4 lg:grid lg:grid-cols-5">
+        <div className="relative z-20 mt-20 p-10 pt-20 text-5xl font-bold text-white lg:col-span-3 lg:p-20 lg:pt-40 lg:text-7xl">
+          <h1>
+            Our Right to the City: <br /> A Festival of Urban Liberation
+          </h1>
         </div>
-        <div className="z-20 row-span-1 flex flex-col justify-start bg-black p-10 text-right text-4xl text-white lg:row-span-2 lg:flex-row lg:justify-end lg:space-x-10 lg:space-y-0 lg:p-20 lg:text-4xl">
-          <a
-            href="#about"
-            className="hover:opacity-70 active:opacity-100"
-          >
+      </div>
+      <div className="z-20 row-span-2 flex flex-col justify-start bg-black p-10 lg:row-span-1 lg:grid lg:grid-cols-5 lg:items-center lg:p-20">
+        <div className="text-3xl text-white lg:col-span-3  lg:text-4xl">
+          <a href="#getInvolved" className="hover:opacity-70">
+            Get Involved
+          </a>
+        </div>
+        <div className="flex flex-col justify-start space-y-5 text-right text-3xl text-white lg:col-span-2 lg:flex-row lg:space-x-10 lg:space-y-0 lg:text-left lg:text-4xl">
+          <a href="#about" className="hover:opacity-70">
             About
           </a>
-          <a
-            href="#whatsOn"
-            className="hover:opacity-70 active:opacity-100"
-          >
+          <a href="#whatsOn" className="hover:opacity-70">
             What's On
           </a>
-          <a
-            href="#location"
-            className="hover:opacity-70 active:opacity-100"
-          >
+          <a href="#location" className="hover:opacity-70">
             Location
           </a>
         </div>
-        <div className="absolute inset-0 z-0 h-screen bg-[url('images/city-bg.jpg')] bg-cover bg-center brightness-50"></div>
       </div>
+      <div className="absolute inset-0 z-0 h-screen bg-[url('images/city-bg.jpg')] bg-cover bg-center brightness-50"></div>
     </div>
   );
 }
--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,20 +1,23 @@
 export default function Location() {
   return (
     <div
-      className="min-h-screen p-10 text-2xl lg:grid lg:grid-cols-5 lg:p-20 lg:text-4xl"
+      className="min-h-screen p-4 text-lg lg:grid lg:grid-cols-5 lg:p-20 lg:text-4xl"
       id="location"
     >
-      <div className="col-span-3 max-w-full">
+      <div className="col-span-3 w-full">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2508105.0866143876!2d-4.760068744374395!3d52.12847532622325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761b56fb64b275%3A0xc756e26675d21f40!2sBarbican%20Centre!5e0!3m2!1sen!2suk!4v1716574856668!5m2!1sen!2suk"
-          width="600"
-          height="450"
           style={{ border: 0 }}
           aria-hidden="false"
-          className="max-w-screen-sm border-none lg:mx-10"
+          className="border-nonelg:mx-10 h-64 w-full"
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
         />
       </div>
-      <div>The event will be held at the Barbican.</div>
+      <div className="col-span-2 flex items-center justify-center text-center lg:text-left">
+        The event will be held at the Barbican.
+      </div>
     </div>
   );
 }
