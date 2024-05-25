a3e040f3970c3e8bd8cdd1c775a90262095ead6e
jones58 <108466184+jones58@users.noreply.github.com>
2024-05-25 18:04:50 +0100

    planning out whats on page

--- src/components/WhatsOn.tsx
+++ src/components/WhatsOn.tsx
@@ -1,11 +1,10 @@
 export default function WhatsOn() {
   return (
     <div
-      className="min-h-screen p-10 text-2xl lg:grid lg:grid-cols-5 lg:p-20 lg:text-4xl"
+      className="min-h-screen p-10 text-2xl lg:p-20 lg:text-4xl"
       id="whatsOn"
     >
-      <div className="col-span-3">Hello</div>
-      <div className="col-span-2">Hello</div>
+      <div className="col-span-3">Events here</div>
     </div>
   );
 }
