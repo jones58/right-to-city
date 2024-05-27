a4caacc417e86d03fb3b7d3fd70f57f002152535
jones58 <108466184+jones58@users.noreply.github.com>
2024-05-27 14:13:20 +0100

    style location element with my-auto

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -13,7 +13,7 @@ export default function Location() {
         families or those who aren't able to camp. This includes some
         accessible rooms.
       </div>
-      <div className="lg:col-span-2">
+      <div className="lg:col-span-2 lg:my-auto">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2508105.0866143876!2d-4.760068744374395!3d52.12847532622325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761b56fb64b275%3A0xc756e26675d21f40!2sBarbican%20Centre!5e0!3m2!1sen!2suk!4v1716574856668!5m2!1sen!2suk"
           aria-hidden="false"
