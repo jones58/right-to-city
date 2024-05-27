7e3513bd28afabeb5d2bf58871cf4242ca336c74
jones58 <108466184+jones58@users.noreply.github.com>
2024-05-27 17:50:17 +0100

    adding layout of location page

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -4,7 +4,7 @@ export default function Location() {
       className="min-h-screen p-10 text-lg lg:grid lg:grid-cols-5 lg:p-20"
       id="location"
     >
-      <div className="prose py-5 lg:col-span-3 lg:py-0 lg:pr-20 lg:text-2xl">
+      <div className="prose py-5 lg:col-span-2 lg:py-0 lg:pr-20 lg:text-2xl">
         <h1 className="py-2 text-3xl font-bold lg:py-5 lg:text-4xl">
           Location
         </h1>
@@ -13,7 +13,7 @@ export default function Location() {
         families or those who aren't able to camp. This includes some
         accessible rooms.
       </div>
-      <div className="lg:col-span-2 lg:my-auto">
+      <div className="lg:col-span-3">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2508105.0866143876!2d-4.760068744374395!3d52.12847532622325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761b56fb64b275%3A0xc756e26675d21f40!2sBarbican%20Centre!5e0!3m2!1sen!2suk!4v1716574856668!5m2!1sen!2suk"
           aria-hidden="false"
