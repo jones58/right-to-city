b1eb38a682e05bbd45771aaa17604772e1394297
jones58 <108466184+jones58@users.noreply.github.com>
2024-05-28 16:33:08 +0100

    edit location and map embed

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -9,20 +9,20 @@ export default function Location() {
           Location
         </h1>
         <p className="prose-xl">
-          The event will be held at the Barbican. Most of our
-          accommodation is in tents, but we do have a number of indoor
-          rooms with beds for families or those who aren't able to camp.
-          This includes some accessible rooms.
+          The event will be held at the Lesnes Estate in Thamesmead. Most
+          of our accommodation is in tents, but we do have a number of
+          indoor rooms with beds for families or those who aren't able to
+          camp. This includes some accessible rooms.
         </p>
       </div>
       <div className="lg:col-span-3">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2508105.0866143876!2d-4.760068744374395!3d52.12847532622325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761b56fb64b275%3A0xc756e26675d21f40!2sBarbican%20Centre!5e0!3m2!1sen!2suk!4v1716574856668!5m2!1sen!2suk"
-          aria-hidden="false"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19868.061729741046!2d0.07361357861590379!3d51.50390074421823!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8af4dd90ae2eb%3A0x434f8c23f47012a7!2sThamesmead%2C%20London!5e0!3m2!1sen!2suk!4v1716910211750!5m2!1sen!2suk"
           className="h-64 w-full border-none lg:h-96"
+          aria-hidden="false"
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
-        />
+        ></iframe>
       </div>
     </div>
   );
