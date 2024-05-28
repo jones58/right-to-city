18b73c1a0102ae5fcc94bf3cb8efafbed52cdd64
jones58 <108466184+jones58@users.noreply.github.com>
2024-05-28 15:13:48 +0100

    edit padding

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -3,14 +3,14 @@ import Form from './Form';
 export default function SignUp() {
   return (
     <div
-      className="min-h-screen p-10 text-2xl lg:grid lg:grid-cols-5 lg:p-20 lg:text-4xl"
+      className="min-h-screen bg-white p-5 text-2xl lg:grid lg:grid-cols-5 lg:p-20 lg:text-4xl"
       id="signUp"
     >
-      <div className="prose lg:col-span-3 lg:pr-20 lg:text-2xl">
-        <h1 className="text-3xl font-bold lg:text-4xl">Sign Up</h1>
+      <div className="lg:col-span-3 lg:pr-20 lg:text-2xl">
+        <h1 className="text-4xl lg:pb-5">Sign Up</h1>
         <Form />
       </div>
-      <div className=" lg:col-span-2">
+      <div className="pt-10 lg:col-span-2 lg:pt-0">
         <img src="/images/city-bg.jpg" className="" />
       </div>
     </div>
