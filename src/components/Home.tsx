38457a8df63a9822220b42984a96028a694b5663
jones58 <108466184+jones58@users.noreply.github.com>
2024-05-28 13:58:51 +0100

    reworking home page styling and changes titles

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -12,20 +12,27 @@ export default function Home() {
         </div>
       </div>
       <nav className="z-20 row-span-2 flex flex-col justify-start bg-black p-10 lg:row-span-1 lg:grid lg:grid-cols-5 lg:items-center lg:p-20">
-        <div className="pb-10 text-3xl text-white lg:col-span-3 lg:pb-0 lg:text-4xl">
-          <a href="#getInvolved" className="hover:opacity-70">
-            Get Involved
-          </a>
-        </div>
+        <a
+          className="z-20 flex h-24 w-24 flex-col justify-center rounded-full bg-red-500 hover:opacity-70 lg:col-span-3 lg:h-40 lg:w-40"
+          href="#CallToAction"
+        >
+          <h1 className="text-center text-xl uppercase text-white lg:text-2xl">
+            Find
+            <br />
+            out
+            <br />
+            more
+          </h1>
+        </a>
         <div className="flex flex-col justify-start space-y-5 text-right text-3xl text-white lg:col-span-2 lg:flex-row lg:justify-between lg:space-x-10 lg:space-y-0 lg:text-left lg:text-4xl">
-          <a href="#about" className="hover:opacity-70">
-            About
-          </a>
           <a href="#whatsOn" className="hover:opacity-70">
             What's On
           </a>
           <a href="#location" className="hover:opacity-70">
-            Location
+            Getting Here
+          </a>
+          <a href="#signUp" className="hover:opacity-70">
+            Sign Up
           </a>
         </div>
       </nav>
