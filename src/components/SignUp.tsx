dc479e1c9adc10159efbd232a370da6dd14ab4eb
jones58 <108466184+jones58@users.noreply.github.com>
2024-05-28 13:55:13 +0100

    rename to sign up

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,13 +1,13 @@
 import Form from './Form';
 
-export default function GetInvolved() {
+export default function SignUp() {
   return (
     <div
       className="min-h-screen p-10 text-2xl lg:grid lg:grid-cols-5 lg:p-20 lg:text-4xl"
-      id="getInvolved"
+      id="signUp"
     >
       <div className="prose lg:col-span-3 lg:pr-20 lg:text-2xl">
-        <h1 className="text-3xl font-bold lg:text-4xl">Get Involved</h1>
+        <h1 className="text-3xl font-bold lg:text-4xl">Sign Up</h1>
         <Form />
       </div>
       <div className=" lg:col-span-2 lg:my-auto">
