52bf1a1c58381cd18177874ef6a04caa4178fc41
jones58 <108466184+jones58@users.noreply.github.com>
2024-05-28 13:58:35 +0100

    adding some text and editing prose style

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,22 +2,35 @@ export default function About() {
   return (
     <div
       className="min-h-screen p-10 text-2xl lg:grid lg:grid-cols-5 lg:p-20 lg:text-4xl"
-      id="about"
+      id="CallToAction"
     >
       <div className=" lg:col-span-3 lg:pr-20">
         <img src="/images/city-2.jpg" />
       </div>
-      <div className="prose py-5 lg:col-span-2 lg:py-0 lg:text-2xl">
+      <div className=" py-5 lg:col-span-2 lg:py-0 lg:text-2xl">
         <h1 className="text-title py-2 text-3xl font-bold lg:py-5 lg:text-4xl">
-          About
+          About the Festival
         </h1>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat
-        harum consectetur dolores commodi necessitatibus fuga libero sed
-        tenetur minus et? Veritatis, autem tempore. Voluptates illum
-        voluptatem ipsa rerum. Harum, laudantium! Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Veniam eius esse ad quis odio
-        id quidem numquam quasi. Totam vero non corporis id suscipit
-        placeat libero voluptatem dolor facilis eum.
+        <p className="prose-xl">
+          The Festival of Urban Liberation will be bringing together
+          campaigns and communities of resistance and struggles from
+          across London.
+          <br />
+          <br /> London is not only a highly neo-liberal city where global
+          capitalism rules, feeding itself the fruits of exploitation both
+          here and through the spoils of colonial wealth, it is also a
+          city of life, of resistance and struggle, of power from below
+          trying to find a way through. From residents fighting demolition
+          of their homes and disrepair, to parents fighting against
+          closures of schools & nurseries. From those challenging climate
+          change, to communities stopping immigration raids, and the
+          victims of the Windrush scandal and systemic racism. <br />{' '}
+          <br />
+          London resists - street by street, block by block - come and
+          unite the struggles, celebrate and let's conspire together for a
+          future liberated London that includes everyone that chooses to
+          make it home, for our collective right to the city.
+        </p>
       </div>
     </div>
   );
--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -4,14 +4,16 @@ export default function Location() {
       className="min-h-screen p-10 text-lg lg:grid lg:grid-cols-5 lg:p-20"
       id="location"
     >
-      <div className="prose py-5 lg:col-span-2 lg:py-0 lg:pr-20 lg:text-2xl">
+      <div className="py-5 lg:col-span-2 lg:py-0 lg:pr-20 lg:text-2xl">
         <h1 className="py-2 text-3xl font-bold lg:py-5 lg:text-4xl">
           Location
         </h1>
-        The event will be held at the Barbican. Most of our accommodation
-        is in tents, but we do have a number of indoor rooms with beds for
-        families or those who aren't able to camp. This includes some
-        accessible rooms.
+        <p className="prose-xl">
+          The event will be held at the Barbican. Most of our
+          accommodation is in tents, but we do have a number of indoor
+          rooms with beds for families or those who aren't able to camp.
+          This includes some accessible rooms.
+        </p>
       </div>
       <div className="lg:col-span-3">
         <iframe
