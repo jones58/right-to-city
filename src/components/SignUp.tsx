d21833b5698836955034e997d4203d74c6e0252e
jones58 <108466184+jones58@users.noreply.github.com>
2024-05-30 14:02:44 +0100

    style changes to sign up form

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -9,7 +9,7 @@ export default function SignUp() {
       <div className="lg:col-span-3 lg:mr-20 lg:text-2xl">
         <Form />
       </div>
-      <div className="pt-10 lg:col-span-2 lg:p-8 lg:pt-0">
+      <div className="pt-10 lg:col-span-2 lg:py-8 lg:pt-0">
         <img
           src="/images/city-3.jpg"
           className="h-full w-full rounded-lg object-cover shadow-lg"
