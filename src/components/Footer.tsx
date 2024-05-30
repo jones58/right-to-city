ee14862b7d01d150a339bbcfe3147a44c6f346b9
jones58 <108466184+jones58@users.noreply.github.com>
2024-05-30 13:49:31 +0100

    small style improvements to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,31 +2,33 @@ import SocialIcons from './social-icons';
 
 export default function Footer() {
   return (
-    <footer className="bg-black-custom text-white-custom flex w-full flex-col justify-between p-5 text-2xl lg:flex-row lg:px-20">
-      <div>
-        <p className="py-10 text-2xl font-bold lg:py-0 lg:pb-10 lg:pt-10">
-          Our Right to the City: A Festival of Urban Liberation
-        </p>
+    <footer className="flex w-full flex-col justify-between bg-black-custom p-5 py-10 text-2xl text-white-custom lg:flex-row lg:px-20">
+      <div className="flex flex-col space-y-9">
+        <a href="#home" className="hover:opacity-70">
+          <h1 className="text-3xl font-bold lg:text-4xl">
+            Our Right to the City: A Festival of Urban Liberation
+          </h1>
+        </a>
         <SocialIcons />
       </div>
-      <div className="py-10 lg:text-right">
+      <div className="mt-10 flex flex-col space-y-3 lg:justify-end lg:text-right">
         <p>
           Site by{' '}
           <a
             href="https://www.jackkershaw.net"
             target="_blank"
-            className="hover:opacity-70 active:opacity-100"
+            className="font-body hover:opacity-70"
             aria-label="link to Jack Kershaw's website"
           >
             Jack Kershaw.
           </a>
         </p>
-        <p className="py-5 lg:py-12">
+        <p>
           Source on{' '}
           <a
             href="https://github.com/jones58/right-to-city"
             target="_blank"
-            className="hover:opacity-70 active:opacity-100"
+            className="font-body hover:opacity-70"
             aria-label="link to the Vital Signs Magazine GitHub repository"
           >
             Github.
