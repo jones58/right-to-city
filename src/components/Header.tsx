6d2db428340c0b440a81912f59b342805164045f
jones58 <108466184+jones58@users.noreply.github.com>
2024-05-30 15:35:54 +0100

    edit header color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="relative z-20 flex flex-col justify-start space-y-5 bg-black-custom bg-opacity-40 p-5 text-right text-3xl text-white-custom lg:col-span-2 lg:flex-row lg:items-center lg:justify-end lg:space-x-10 lg:space-y-0 lg:px-20 lg:py-10 lg:text-left lg:text-4xl">
+    <header className="relative z-20 flex flex-col justify-start space-y-5 bg-blue-custom bg-opacity-70 p-5 text-right text-3xl text-white-custom lg:col-span-2 lg:flex-row lg:items-center lg:justify-end lg:space-x-10 lg:space-y-0 lg:px-20 lg:py-10 lg:text-left lg:text-4xl">
       <a href="#about" className="hover:opacity-70">
         About
       </a>
