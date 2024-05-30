5d434479070cc67059ef7fecbc92384e7f9c546a
jones58 <108466184+jones58@users.noreply.github.com>
2024-05-30 15:35:43 +0100

    edit event card image rounding

--- src/components/eventCard.tsx
+++ src/components/eventCard.tsx
@@ -16,13 +16,11 @@ export default function EventCard({
   return (
     <div className="flex max-w-md flex-col justify-between rounded-lg bg-white-custom p-5 text-black-custom shadow-lg ">
       <img
-        className="h-32 w-full object-cover object-center"
+        className="h-32 w-full rounded-lg object-cover object-center"
         src={imageUrl}
         alt={title}
       />
-      <h1 className="h-20 pt-5 text-2xl font-bold capitalize lg:pt-2">
-        {title}
-      </h1>
+      <h1 className="h-20 pt-5 text-2xl font-bold  lg:pt-2">{title}</h1>
       <div className="text-right">
         <p className="text-xl">{location}</p>
         <p className="text-xl">{time}</p>
