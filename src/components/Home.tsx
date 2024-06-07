26d7a26ccfa1edca24327f12982f2809f591c96d
jones58 <108466184+jones58@users.noreply.github.com>
2024-06-07 15:05:15 +0100

    small changes to home image

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div id="home" className="h-screen">
       <Header />
       <div className="lg:grid lg:grid-cols-5">
-        <div className="relative z-20 mt-10 p-5  py-10 pt-20 text-5xl font-bold uppercase text-white-custom lg:col-span-3 lg:p-20 lg:pt-40 lg:text-7xl">
+        <div className="relative z-20 mt-10 p-5 py-10 pt-20 text-5xl font-bold uppercase text-white-custom lg:col-span-3 lg:p-20 lg:pt-40 lg:text-7xl">
           <h1>
             <h1 className="font-greve-wide-bold">
               Our Right to the City:
@@ -14,7 +14,7 @@ export default function Home() {
           </h1>
         </div>
       </div>
-      <div className="absolute inset-0 z-0 h-screen bg-[url('/images/city-bg.jpg')] bg-cover bg-center">
+      <div className="absolute inset-0 z-0 h-screen bg-[url('/images/city-bg.jpg')] bg-cover bg-center brightness-50">
         <div className="sr-only">
           The image depicts a large protest or demonstration taking place
           in an urban setting. A dense crowd of people can be seen holding
